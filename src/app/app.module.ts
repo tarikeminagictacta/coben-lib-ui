fa8f00ff80a5adc16c8c44bd1ead8b4ff8f3c5d0
Tarik Eminagic <tarik.eminagic@cegeka.com>
2019-04-09 18:22:49 +0200

    feature(translate): added translation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { CobenLibCommonModule } from '@coben-lib/common';
 import { CobenLibFinanceModule } from '@coben-lib/finance';
 import { CobenLibHospiModule } from '@coben-lib/hospi';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SomeComponentComponent } from './some-component/some-component.component';
 
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    SomeComponentComponent
-  ],
+  declarations: [AppComponent, SomeComponentComponent],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: (createTranslateLoader),
+        deps: [HttpClient]
+      }
+    }),
     CobenLibCommonModule,
     CobenLibHospiModule,
     CobenLibFinanceModule,
@@ -23,4 +35,4 @@ import { SomeComponentComponent } from './some-component/some-component.componen
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
